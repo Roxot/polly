885984e2d5d2f3bf780d80dcece0f8b5fe7a44ff
Bryan Eikema <bryan.eikema@student.uva.nl>
2015-02-26 20:01:48 +0100

    POST POLL now also returns the creator's phone number and display name

--- src/polly/httpserver/poll.go
+++ src/polly/httpserver/poll.go
@@ -53,6 +53,8 @@ func (srv *HTTPServer) PostPoll(w http.ResponseWriter, r *http.Request,
 	// }
 
 	pollData.Creator.Id = user.Id
+	pollData.Creator.PhoneNumber = user.PhoneNumber
+	pollData.Creator.DisplayName = user.DisplayName
 	pollData.MetaData.CreationDate = time.Now().Unix()
 
 	// Validate poll (non-empty title, no votes (?), etc.)
