4e404ac9c77a40dcee05c17f47fdb6ed4cfa3a12
Bryan Eikema <bryan.eikema@student.uva.nl>
2016-04-01 17:41:14 -0400

    More stuffz

--- http/poll_test.go
+++ http/poll_test.go
@@ -1,50 +1,69 @@
 package http
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"testing"
-	"time"
-
-	"github.com/roxot/polly"
 )
 
-func BenchmarkPostPoll(b *testing.B) {
-	pollMsg := polly.PollMessage{
-		MetaData: polly.Poll{
-			ClosingDate: time.Now().Add(time.Hour).UnixNano() / 1000000,
-		},
-		Question: polly.Question{
-			Type:  polly.QUESTION_TYPE_MC,
-			Title: "What is the answer?",
-		},
-		Options: []polly.Option{
-			polly.Option{
-				Value: "This is the answer",
-			},
-		},
-		Participants: []polly.PublicUser{
-			polly.PublicUser{
-				ID: testUserID,
-			},
-		},
-	}
+// func BenchmarkPostPoll(b *testing.B) {
+// 	pollMsg := polly.PollMessage{
+// 		MetaData: polly.Poll{
+// 			ClosingDate: time.Now().Add(time.Hour).UnixNano() / 1000000,
+// 		},
+// 		Question: polly.Question{
+// 			Type:  polly.QUESTION_TYPE_MC,
+// 			Title: "What is the answer?",
+// 		},
+// 		Options: []polly.Option{
+// 			polly.Option{
+// 				Value: "This is the answer",
+// 			},
+// 		},
+// 		Participants: []polly.PublicUser{
+// 			polly.PublicUser{
+// 				ID: testUserID,
+// 			},
+// 		},
+// 	}
 
-	body, err := json.Marshal(&pollMsg)
+// 	body, err := json.Marshal(&pollMsg)
+// 	if err != nil {
+// 		b.Fatal(err)
+// 	}
+
+// 	api := fmt.Sprintf(cEndpointFormat, cAPIVersion, "poll")
+// 	client := new(http.Client)
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		request, err := createAuthenticatedRequest("POST", api, body)
+// 		if err != nil {
+// 			b.Fatal(err)
+// 		}
+
+// 		response, err := client.Do(request)
+// 		if err != nil || response.StatusCode != http.StatusOK {
+// 			b.Fatal(err)
+// 		}
+// 	}
+// }
+
+func BenchmarkGetPollBulk(b *testing.B) {
+	pollMsg, err := insertDummyPoll()
 	if err != nil {
-		b.Fatal(err)
+		log.Fatal(err)
 	}
 
-	api := fmt.Sprintf(cEndpointFormat, cAPIVersion, "poll")
+	api := fmt.Sprintf(cEndpointFormat+"?id=%d", cAPIVersion, "polls", pollMsg.MetaData.ID)
+	request, err := createAuthenticatedRequest("GET", api, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	client := new(http.Client)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		request, err := createAuthenticatedRequest("POST", api, body)
-		if err != nil {
-			b.Fatal(err)
-		}
-
 		response, err := client.Do(request)
 		if err != nil || response.StatusCode != http.StatusOK {
 			b.Fatal(err)
